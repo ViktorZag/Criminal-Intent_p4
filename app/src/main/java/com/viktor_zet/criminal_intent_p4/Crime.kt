857d2ec010cc6e32b25107037ba1c4ffd830493c
Viktor <viktorzahurskiy@gmail.com>
2022-01-31 19:40:05 +0200

    Classes added, layouts created

--- app/src/main/java/com/viktor_zet/criminal_intent_p4/Crime.kt
+++ app/src/main/java/com/viktor_zet/criminal_intent_p4/Crime.kt
@@ -8,5 +8,5 @@ data class Crime(
     var date: Date = Date(),
     var isSolved: Boolean = false
 ) {
-    
+
 }
--- app/src/main/java/com/viktor_zet/criminal_intent_p4/CrimeFragment.kt
+++ app/src/main/java/com/viktor_zet/criminal_intent_p4/CrimeFragment.kt
@@ -1,12 +1,14 @@
 package com.viktor_zet.criminal_intent_p4
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
-class MainActivity : AppCompatActivity() {
+class CrimeFragment : Fragment() {
+
+    private lateinit var crime: Crime
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        crime = Crime()
     }
 }
