29c0addab7d0630a62417dd21d0df22ff1f8f1dc
Viktor <viktorzahurskiy@gmail.com>
2022-02-01 23:42:46 +0200

    Added getters into repository

--- app/src/main/java/com/viktor_zet/criminal_intent_p4/repository/CrimeRepository.kt
+++ app/src/main/java/com/viktor_zet/criminal_intent_p4/repository/CrimeRepository.kt
@@ -1,18 +1,38 @@
 package com.viktor_zet.criminal_intent_p4.repository
 
 import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import com.viktor_zet.criminal_intent_p4.Crime
+import com.viktor_zet.criminal_intent_p4.database.CrimeDatabase
 import java.lang.IllegalStateException
+import java.util.*
 
-class CrimeRepository private constructor(context: Context){
+private const val DATABASE_NAME = "crime_database"
 
-    companion object{
-        private var INSTANCE: CrimeRepository?=null
+class CrimeRepository private constructor(context: Context) {
 
-        fun initialize(context: Context){
-            if (INSTANCE ==null) INSTANCE = CrimeRepository(context)
+    private val database: CrimeDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        CrimeDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    private val crimeDao = database.crimeDao()
+
+    fun getCrimes(): List<Crime> = crimeDao.getCrimes()
+
+    fun getCrime(id: UUID): Crime? = crimeDao.getCrime(id)
+
+    companion object {
+        private var INSTANCE: CrimeRepository? = null
+
+        fun initialize(context: Context) {
+            if (INSTANCE == null) INSTANCE = CrimeRepository(context)
         }
+
         fun get(): CrimeRepository {
-            return INSTANCE ?:throw IllegalStateException("Crime repository must be initialized")
+            return INSTANCE ?: throw IllegalStateException("Crime repository must be initialized")
         }
     }
 }
